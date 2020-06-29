6dbbb94f57cd01e1555c3229812ae0779a6a69fb
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-06-29 17:19:54 +0800

    feat: done for the content of ability section

---
+++ src/pages/Landing/AbilitySection/data.js
@@ -0,0 +1,25 @@
+const data = {
+    languages: [
+        { name: 'Mandarin', score: 99 },
+        { name: 'English', score: 60 }
+    ],
+    programming: [
+        { name: 'Javascript', score: 90 },
+        { name: 'React', score: 75 },
+        { name: 'Redux', score: 65 },
+        { name: 'NodeJs', score: 65 },
+        { name: 'Git', score: 70 },
+        { name: 'GraphQL', score: 50 },
+        { name: 'PostgreSQL', score: 60 },
+        { name: 'Scss', score: 80 },
+        { name: 'Docker', score: 40 },
+        { name: 'Kubernetes', score: 15 },
+    ],
+    others: [
+        { name: 'Management', score: 60 },
+        { name: 'Design', score: 10 },
+        { name: 'Sketch', score: 10 },
+    ]
+}
+
+export default data;
--- src/pages/Landing/AbilitySection/index.js
+++ src/pages/Landing/AbilitySection/index.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import { ProgressBar } from 'react-bootstrap';
 import './styles.scss';
+import data from './data';
 
+const languages = data.languages;
+const programming = data.programming;
+const others = data.others;
 const AbilitySection = () => {
+    const renderProgressBar = (title, data) => {
+        return (
+            <React.Fragment>
+                <div className="title">{title}</div>
+                { data.map((item) => (
+                    <React.Fragment>
+                        <label>{item.name}</label>
+                        <ProgressBar now={item.score} />
+                    </React.Fragment>
+                )) }
+            </React.Fragment>
+        )
+    }
     return (
         <section className="ability-outer-container">
             <div className="ability-inner-container">
                 <h2>Abilities</h2>
-                <div className="title">Language</div>
-                <label>Mandarin 99%</label>
-                <ProgressBar now={99} style={{height:"10px !important"}} />
-                <label>English 60%</label>
-                <ProgressBar now={60} style={{height:"10px !important"}} />
-                <div className="title">Programming</div>
-                <label>Javascript 40%</label>
-                <ProgressBar now={40} style={{height:"10px !important"}} />
-                <label>Javascript 40%</label>
-                <ProgressBar now={40} style={{height:"10px !important"}} />
-                <label>Javascript 40%</label>
-                <ProgressBar now={40} style={{height:"10px !important"}} />
-                <label>Javascript 40%</label>
-                <ProgressBar now={40} style={{height:"10px !important"}} />
-                <label>Javascript 40%</label>
-                <ProgressBar now={40} style={{height:"10px !important"}} />
-                <div className="title">Other</div>
-                <label>Design 10%</label>
-                <ProgressBar now={10} style={{height:"10px !important"}} />
-                <label>Sketch 20%</label>
-                <ProgressBar now={20} style={{height:"10px !important"}} />
+                {renderProgressBar('Languages', languages)}
+                {renderProgressBar('Programming', programming)}
+                {renderProgressBar('Others', others)}
             </div>
         </section>
     );
