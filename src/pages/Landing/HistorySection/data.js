6e19d233f76b97bbbd3c2eb5e3434a598e0574ea
Dylan Yang <dogi9527dogi9527@gmail.com>
2020-07-03 12:28:14 +0800

    feat: update the content of history section

--- src/pages/Landing/HistorySection/data.js
+++ src/pages/Landing/HistorySection/data.js
@@ -7,20 +7,13 @@ const data = {
             link: 'https://codegiant.io/home',
             highlights: [
                 'Say goodbye to git flow and embrace the trunk based development.',
-                'Read git protocol, handshake, hook and file pack.',
-                'Have a memorable time to play with Ruby on Rails.',
-                'Integrate DB from MongoDB & SQLite to PostgreSQL.',
+                'Hmm, git protocol, handshake, hook and file pack, yummy.',
+                'Had a memorable time to play with Ruby on Rails.',
+                'Integrated DB from MongoDB & SQLite to PostgreSQL.',
                 'Hiring people is not an easy job.'
             ],
-            tags: ['ROR', 'GraphQL', 'PostgreSQL', 'React', 'Git', 'Python', 'Linux', 'Docker', 'MongoDB', 'Style Components', 'Scrum', 'Webpack', 'Slack'],
-            desc: "Codegiant is a git resources platform.",
-            detail: "Codegiant is a git resources platform. Inside the Codegiant, we run the scrum every day. \
-            Except for the main duty, like learning and coding in React, Ruby on Rails and Graphql Ruby, \
-            co-working with our team members is one of my favorite things, especially when we finally figure out a way to solve a problem. \
-            No matter what the problem is, the main thing is all about to let the team can keep running fast. \
-            Refactor the whole system structure is another big thing for me. Have the discussion with our DevOp teammate and implement the solution to see our system get optimized makes me feel excited. \
-            During my time in the Codegiant, I also took around 2 months to handle another platform call Talentherder. \
-            In that project, what impressed me the most is data migration process. To integrate Database from SQLite & MongoDB to PostgreSQL with Python Jupyter Lib is an interesting challenge!"
+            tags: ['Ruby on Rails', 'GraphQL', 'PostgreSQL', 'React', 'Git', 'Python', 'Jupyter', 'Linux', 'Docker', 'MongoDB', 'Styled Components', 'Scrum', 'Webpack', 'Slack'],
+            desc: "Codegiant is a git resources platform."
         },
         {
             name: 'Catchplay',
@@ -28,19 +21,14 @@ const data = {
             position: 'Front-end Engineer',
             link: 'https://www.catchplay.com/',
             highlights: [
-                'Payment system, you need to keep in acumen for everything.',
+                'Payment system, you had to keep in acumen for everything.',
                 'GTM is my friend with our analysts.',
-                'Add "Series" feature is a huge change!',
-                'The war room is memorable. It will be closed when we win the game.',
+                '"TV Series" is a big feature!',
+                'The developer war room is astonishing. It will be closed when we win the game.',
                 'Hooray! A magic campaign website controlled by CMS.'
             ],
-            tags: ['React', 'Redux', 'GTM', 'Git', 'Webpack', 'Slack', 'NodeJS'],
-            desc: "Catchplay is an OTT company.",
-            detail: "Catchplay is an OTT company. My main duty includes refactoring, revision, and optimization for the web front-end platform. \
-            Have to co-working with our designers to provide a better user interface, and also cooperate with our data analysts to track the users' footprints with GTM on our platform. \
-            Integrate and create a reusable modularized component for our payment system is one of my major focusing things. \
-            To clarify and categorize the payment services from Taiwan, Singapore, and Indonesia is one of my jobs. \
-            Those services have to combine with our price plans, then the permutations come out. To organize and modularize the permutation results is going to be an interesting topic."
+            tags: ['React', 'Redux', 'GTM', 'Git', 'Webpack', 'Slack', 'NodeJs'],
+            desc: "Catchplay is an OTT company."
         },
         {
             name: 'Fun.Crea.Free',
@@ -48,17 +36,14 @@ const data = {
             position: 'Co-Founder',
             link: '',
             highlights: [
-                'Participate in projects from planning, scheduling to executing.',
-                'Communication with clients is like playing chess.',
-                'Time control and cherish partners are important.',
-                'You have almost to do everything as a Co-Founder, even it\'s small.'
+                'Standardized the workflow SOP for the team.',
+                'Demand interview is an interesting process.',
+                'Team mates are important!',
+                'You have almost to do everything as a Co-Founder.',
+                'Let\'s try again.'
             ],
-            tags: ['Laravel', 'AWS EC2', 'Sketch', 'Git', 'SCSS', 'NodeJS', 'Project Management'],
-            desc: "Fun.Crea.Free is an outsourcing company.",
-            detail: "Fun.Crea.Free is a project type company. We help our clients to create the best website for their users. \
-            Here, you get to be the front line hero, communicate and discuss with the client, designer, and marketer. \
-            Once both sides get the consensus, the next thing is to build the timeline and allocate the works properly. \
-            Here is the place that I learned how to basically use the Sketch, and the place I inhaled some design cells into my blood."
+            tags: ['Laravel', 'AWS EC2', 'Sketch', 'Git', 'SCSS', 'NodeJS', 'Management'],
+            desc: "Fun.Crea.Free is an outsourcing company."
         },
         {
             name: 'Tiger Party',
@@ -66,18 +51,14 @@ const data = {
             position: 'Engineer',
             link: 'https://thetigerparty.com/en',
             highlights: [
-                'Maintain the billboard system in NYC Times Square, you need to be bolder.',
-                'Be an app developer needs more patient than be a web developer.',
-                'Providing technical support to end-users in English.',
-                'More critical, more risks, more excited.'
+                'Warning! Handle the billboard system which is running alive.',
+                'Be an app developer needs more patient',
+                'Providing technical support in English is excited.',
+                'More critical, more risks, more exciting.',
+                'Trust is important.'
             ],
-            tags: ['Android', 'XCode', 'PHP', 'Cordova', 'Git', 'CI/CD', 'Router'],
-            desc: "Tiger Party focus on digital billboard system.",
-            detail: "Tiger Party's main product is the digital billboard system. \
-            In Tiger, I had a chance to wrote products in many different kinds of programming languages, like Android, XCode, PHP, and ... \
-            The luckiest thing is to be a support engineer for our main product. \
-            I feel so grateful this is the first company that I joined after I graduated. \
-            Many thanks to my colleagues and managers."
+            tags: ['Android', 'XCode', 'PHP', 'Cordova', 'Git', 'CI/CD', 'Router Machine', 'NYC'],
+            desc: "Tiger Party focus on digital billboard system."
         },
     ]
 }
